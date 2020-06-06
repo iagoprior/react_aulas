cbe596aa6470ec234e18300ffee8327ec877f61d
Iago Prior <iagoi.a.s.p_@hotmail.com>
2020-06-06 16:16:55 -0300

    adicionando webpack com babel

---
+++ webpack-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from './Button'
+
+const App = () => {
+  return (
+    <div>
+        <p>Digital Innovation One</p>
+        <p>Aula de Webpack</p>
+        <p>Alterando o app</p>
+        <Button />
+    </div>
+  );
+};
+export default App;
---
+++ webpack-app/src/Button.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Button = () => (
+    <button>Clique Aqui</button>
+)
+
+export default Button
